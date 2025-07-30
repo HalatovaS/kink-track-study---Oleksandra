92f68bb7f179fae0376551d296897aa25811996a
Oleksandra Halatova <halatovas@knu.ua>
2025-07-30 11:58:47 +0200

    resim

--- Bi207/send.sh
+++ Bi207/send.sh
@@ -12,6 +12,7 @@
 FAL_DIR=/sps/nemo/sw/redhat-9-x86_64/snsw/opt2/falaise-5.1.5/bin
 CONF_FAL=/sps/nemo/sw/Falaise/install_develop/share/Falaise-4.1.0/resources/snemo/demonstrator/reconstruction/
 MY_DIR=/sps/nemo/scratch/ohalatova/kink_track_study/Bi207
+SW_DIR=/sps/nemo/scratch/ohalatova/softwares
 
 source ${THRONG_DIR}/config/supernemo_profile.bash   # load Falaise
 snswmgr_load_setup falaise@5.1.5
@@ -20,6 +21,6 @@ ${FAL_DIR}/flsimulate -c ${MY_DIR}/simu_setup.conf -o ${MY_DIR}/simu_Bi.brio
 
 ${FAL_DIR}/flreconstruct -i ${MY_DIR}/simu_Bi.brio -p ${CONF_FAL}/official-2.0.0.conf -o ${MY_DIR}/reco_Bi.brio
 
-${FAL_DIR}/flreconstruct -i ${MY_DIR}/reco_Bi.brio -p /sps/nemo/scratch/ohalatova/softwares/CimrmanModule/reco.conf -o ${MY_DIR}/CM_reco_Bi.brio
+${FAL_DIR}/flreconstruct -i ${MY_DIR}/reco_Bi.brio -p ${SW_DIR}/CimrmanModule/reco.conf -o ${MY_DIR}/CM_reco_Bi.brio
 
-# ${FAL_DIR}/flreconstruct -i ${MY_DIR}/CM_reco_Bi.brio -p /sps/nemo/scratch/ohalatova/softwares/SNCuts/build/SNCutsPipeline.conf -o ${MY_DIR}/SNC_reco_Bi.brio
+# ${FAL_DIR}/flreconstruct -i ${MY_DIR}/CM_reco_Bi.brio -p ${SW_DIR}/SNCuts/build/SNCutsPipeline.conf -o ${MY_DIR}/SNC_reco_Bi.brio
