69e61f379ffb6e04b1f0d04607f310953f147410
Oleksandra Halatova <halatovas@knu.ua>
2025-08-01 14:07:23 +0200

    cleaning

--- Bi207/send.sh
+++ Bi207/send.sh
@@ -10,7 +10,6 @@
 #SBATCH --cpus-per-task=1
 
 FAL_DIR=/sps/nemo/sw/redhat-9-x86_64/snsw/opt2/falaise-5.1.5/bin
-CONF_FAL=/sps/nemo/sw/Falaise/install_develop/share/Falaise-4.1.0/resources/snemo/demonstrator/reconstruction/
 MY_DIR=/sps/nemo/scratch/ohalatova/kink_track_study/Bi207
 SW_DIR=/sps/nemo/scratch/ohalatova/softwares
 
@@ -18,8 +17,6 @@ source ${THRONG_DIR}/config/supernemo_profile.bash   # load Falaise
 snswmgr_load_setup falaise@5.1.5
 
 ${FAL_DIR}/flsimulate -c ${MY_DIR}/simu_setup.conf -o ${MY_DIR}/simu_Bi.brio
-
-# ${FAL_DIR}/flreconstruct -i ${MY_DIR}/simu_Bi.brio -p ${CONF_FAL}/official-2.0.0.conf -o ${MY_DIR}/reco_Bi.brio
 
 ${FAL_DIR}/flreconstruct -i ${MY_DIR}/simu_Bi.brio -p ${MY_DIR}/reco.conf -o ${MY_DIR}/CM_reco_Bi.brio
 
