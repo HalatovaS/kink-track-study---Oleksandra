7029bd4ec47a3f14489526331373bf07547820a6
Oleksandra Halatova <halatovas@knu.ua>
2025-08-04 14:32:20 +0200

    PTD is back, but no pCD

--- Bi207/send.sh
+++ Bi207/send.sh
@@ -11,6 +11,7 @@
 
 FAL_DIR=/sps/nemo/sw/redhat-9-x86_64/snsw/opt2/falaise-5.1.5/bin
 MY_DIR=/sps/nemo/scratch/ohalatova/kink_track_study/Bi207
+CONF_FAL=/sps/nemo/sw/Falaise/install_develop/share/Falaise-4.1.0/resources/snemo/demonstrator/reconstruction
 # SW_DIR=/sps/nemo/scratch/ohalatova/softwares
 
 source ${THRONG_DIR}/config/supernemo_profile.bash  
@@ -18,7 +19,9 @@ snswmgr_load_setup falaise@5.1.5
 
 ${FAL_DIR}/flsimulate -c ${MY_DIR}/simu_setup.conf -o ${MY_DIR}/simu_Bi.brio
 
-${FAL_DIR}/flreconstruct -i ${MY_DIR}/simu_Bi.brio -p ${MY_DIR}/reco.conf -o ${MY_DIR}/CM_reco_Bi.brio
+${FAL_DIR}/flreconstruct -i ${MY_DIR}/simu_Bi.brio -p ${CONF_FAL}/official-2.0.0.conf -o ${MY_DIR}/PTD_Bi.brio
+
+${FAL_DIR}/flreconstruct -i ${MY_DIR}/PTD_Bi.brio -p ${MY_DIR}/reco.conf -o ${MY_DIR}/CM_reco_Bi.brio
 
 ${FAL_DIR}/flreconstruct -i ${MY_DIR}/CM_reco_Bi.brio -p ${MY_DIR}/calibration_cuts.conf -o ${MY_DIR}/EnCalib_Bi.brio
 
