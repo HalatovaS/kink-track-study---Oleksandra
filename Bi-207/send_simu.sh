d8e35959d1362d8afea95143c9e506bc11c7ed5a
Oleksandra Halatova <halatovas@knu.ua>
2025-08-12 13:27:02 +0200

    Before simulating 1 000 000 events

--- Bi-207/send_simu.sh
+++ Bi-207/send_simu.sh
@@ -21,3 +21,4 @@ ${FAL_DIR}/flsimulate -c ${MY_DIR}/simu_setup.conf -o ${MY_DIR}/DATA/$num/simu_B
 
 ${FAL_DIR}/flreconstruct -i ${MY_DIR}/DATA/$num/simu_Bi_$num.brio -p ${MY_DIR}/reco.conf -o ${MY_DIR}/DATA/$num/CM_reco_Bi_$num.brio
 
+rm -f ${MY_DIR}/DATA/$num/simu_Bi_$num.brio     # -f ensures it will not complain if file doen't exist
